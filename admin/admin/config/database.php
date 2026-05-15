7d9d6b1d6959af5b5120256a2fdd34df046a04bd
turaybaimba404-oss <turaybaimba404@gmail.com>
2026-05-15 21:40:14 +0000

    database credentials

--- admin/admin/config/database.php
+++ admin/admin/config/database.php
@@ -1,20 +1,23 @@
 <?php
-define('DB_HOST', getenv('DB_HOST'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASS', getenv('DB_PASS'));
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_HOST', '01s3vb.h.filess.io');
+define('DB_PORT', '3307');
+define('DB_USER', 'votingsystem_entirefox');
+define('DB_PASS', 'f27938827ce05ebb28d2b8660856d00d44afb229');
+define('DB_NAME', 'votingsystem_entirefox');
 
 function connectDB() {
     $conn = new mysqli(
         DB_HOST,
         DB_USER,
         DB_PASS,
-        DB_NAME
+        DB_NAME,
+        DB_PORT
     );
     if($conn->connect_error) {
         die(json_encode([
             'success' => false,
-            'message' => 'Database connection failed'
+            'message' => 'Database connection failed: ' 
+                . $conn->connect_error
         ]));
     }
     $conn->set_charset('utf8mb4');
